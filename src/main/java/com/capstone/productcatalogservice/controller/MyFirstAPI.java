43321e666f967e09998ef857b709aca235ee984a
aravind <aravindmalig@gmail.com>
2024-04-15 20:18:50 -0700

    First time pushing it to Git

---
+++ src/main/java/com/capstone/productcatalogservice/controller/MyFirstAPI.java
@@ -0,0 +1,14 @@
+package com.capstone.productcatalogservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstAPI {
+
+    @GetMapping
+    public String MyFirstAPI_call(){
+        return "Hello World!";
+    }
+
+}
